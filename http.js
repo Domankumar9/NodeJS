cc2b08d73f4b8f967574c2d1275370cf844a4d13
Domankumar9 <saidoman09@gmail.com>
2024-04-07 15:18:42 +0530

    testing responce header without html output

--- http.js
+++ http.js
@@ -3,7 +3,7 @@ const http = require('http');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   // Set the response HTTP header with status code and content type
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
   
   // Send the response body
   res.end('Hello, World!\n');
